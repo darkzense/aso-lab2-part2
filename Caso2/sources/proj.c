b8d6ce7e2117e59e1ba6f0238034099c5b1c1b17
darkzense <torresjohn094@gmail.com>
2023-10-11 17:28:40 -0600

    Initial commit

---
+++ Caso2/sources/proj.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../miIncludes/aritReal.h"
+
+int main(int argc, char **argv)
+{
+  float a,b,c,d;
+
+  a= suma(3.5,3.4);
+  b= resta(4.0,2.0);
+  c= multiplica(3.0,5.0);
+  d= divide(7.0,3.0);
+
+  fprintf(stdout,"a= %f\n b= %f \n c= %f \n d= %f\n", a, b, c, d);
+}
---
+++ Caso3/miIncludes/aritReal.h
@@ -0,0 +1,11 @@
+/* archivo 'aritReal' es  almacenado en el directorio 'miIncludes'  */
+
+/* función que suma dos números. */
+float suma(float a, float b); 
+/* función que resta 'b' a 'a' */
+float resta(float a, float b);
+
+/* función que multiplica dos números */
+float multiplica(float a, float b);
+
+float divide(float a, float b);
